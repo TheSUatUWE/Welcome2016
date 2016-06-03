6ffaf2628c1240e950bd8d32309af5c23d1c90a9
Luke Bailey <Luke.Bailey@uwe.ac.uk>
2016-06-03 15:06:29 +0100

    Add responsive menu styles for pages with min-width 1220px

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,9 +9,35 @@
   		$('.sidebar-toggle').click(function(e) {
 	        e.preventDefault();
 	        $('#sidebar-wrapper').toggleClass('toggled');
-	        $(this).toggleClass('toggled');
+	        $('.sidebar-toggle-top').toggleClass('toggled');
+          $(this).toggleClass('toggled');
 	    });
+      $('.sidebar-toggle-top').click(function(e) {
+          e.preventDefault();
+          $('#sidebar-wrapper').toggleClass('toggled');
+          $('.sidebar-toggle').toggleClass('toggled');
+          $(this).toggleClass('toggled');
+      });
   	});
+
+    // Add scrolled class to header 
+    $( document ).ready(function() {
+      if ($(window).scrollTop() !== 0) {
+        $('.navbar-co').addClass('scrolled');
+      }
+
+      $(window).scroll(function() {
+          if ($(document).scrollTop() > 1) {
+            
+            $('nav.nav').addClass('scrolled');
+
+          } else {
+            
+            $('nav.nav').removeClass('scrolled');
+            
+          }
+        });
+    });
 
 
 
