2f92e217bf31d542e061b2ee4763acd81f3c3b0d
Luke Bailey <hello@lukebaielcreative.com>
2016-07-18 15:06:12 +0100

    Add responsive styles for landing section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,25 @@
   'use strict';
 
   $(function () {
+
+    // Smooth Scroll
+
+    $( document ).ready(function() {
+        $(function() {
+          $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
+        });
+    });
     
     //  AJAX MailChimp Sign up //
     $(document).ready(function(){
