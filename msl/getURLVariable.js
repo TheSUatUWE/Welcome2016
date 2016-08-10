467003cb4cc9caa973cbffde8d55bb5a0cf37d1e
Luke Bailey <hello@lukebaielcreative.com>
2016-08-10 15:22:11 +0100

    Add support for signups with same name

--- msl/getURLVariable.js
+++ msl/getURLVariable.js
@@ -9,16 +9,27 @@ $( document ).ready(function() {
 	}
 
 	var placeholder = $('.sign-up-placeholder')
-	var event = getUrlVars()["event"];
+	var eventName = getUrlVars()["event"];
+	var eventID = getUrlVars()["id"];
 
-	event = event.replace(/_/g," ");
+	eventName = eventName.replace(/_/g," ");
 
-	curEventTitle = $('dl.msl_signup dt:contains("' + event + '")');
+	eventNameID = eventName + ' ' + eventID;
+
+	console.log(eventNameID);
+
+	curEventTitle = $('dl.msl_signup dt:contains("' + eventNameID + '")');
 
 	curEvent = curEventTitle.parent('.msl_signup');
 
 	curEvent.addClass('selected').appendTo(placeholder);
 
+	
+
+	var allTitles = $('dl.msl_signup dt');
+	var lastIndex = allTitles.lastIndexOf(" ");
+
+	allTitles = allTitles.substring(0, lastIndex);
 
 	
 
--- msl/signUpURLVariable.js
+++ msl/signUpURLVariable.js
@@ -2,10 +2,12 @@ $( document ).ready(function() {
     $("button.signup-btn").click(function(event) {
 
     	event.preventDefault();
+    	var eID = window.location.pathname.split('/').filter(function(el){ return !!el; }).pop();
+    	console.log(eID);
     	var e = $('.event-details>h1');
     	var e = e.html();
     	var e = e.replace(/ /g,"_");
-    	window.location.href = 'http://www.thestudentsunion.co.uk/welcome2016/signups?event=' + e;
+    	window.location.href = 'http://www.thestudentsunion.co.uk/welcome2016/signups?event=' + e +'&id=' + eID;
 
     });
 });
